8b7fb121eef25d47ecb338ac7762f9cdaf422943
NITIN KISHOOR S <nitinkishoor.s@gmail.com>
2026-07-16 01:49:51 +0530

    Time: 49 ms (39.73%), Space: 19.2 MB (63.98%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,11 +2,12 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         a=[]
         nitin = 0
-        for i in range(32):
+        '''for i in range(32):
             a.append(str(n % 2))
-            n//=2
-        for master in a:
-            nitin = (nitin << 1) | (master =="1")
+            n//=2'''
+        for i in range (32):
+            nitin = (nitin << 1) | (n & 1)
+            n>>=1
         return nitin
 
 
