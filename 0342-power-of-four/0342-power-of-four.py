790da9ca285708564ff3fb272932f2d0a5468cd8
NITIN KISHOOR S <nitinkishoor.s@gmail.com>
2026-07-17 03:04:11 +0530

    Time: 2 ms (18.68%), Space: 19.3 MB (22.49%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==1:
+             return True
+        for i in range(31):
+             if 4**i ==n:
+                return True
+        return False
+        
